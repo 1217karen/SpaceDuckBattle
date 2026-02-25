7c48dd5cca463c73b26a686f4be29e52695151b3
1217karen <k.u.karen.pi@gmail.com>
2026-02-25 17:12:58 +0900

    Create skills.js

---
+++ js/skills.js
@@ -0,0 +1,15 @@
+export const skillHandlers = {
+
+  attack_front1: {
+
+    canUse(unit, target) {
+      // 前方1マスチェック
+    },
+
+    execute(unit, target, log) {
+      // ダメージ処理
+    }
+
+  }
+
+};
