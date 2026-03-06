64984d1277e8fd4307a3a989f19c30b661686d6e
1217karen <k.u.karen.pi@gmail.com>
2026-03-06 15:22:08 +0900

    Create effects-config.js

---
+++ js/effects-config.js
@@ -0,0 +1,77 @@
+//effects-config.js
+
+export const EFFECTS = {
+
+  corrosion: {
+    name: "侵食",
+    group: "debuff",
+    stack: "stock"
+  },
+
+  repair: {
+    name: "修復",
+    group: "buff",
+    stack: "stock"
+  },
+
+  interference: {
+    name: "妨害",
+    group: "debuff",
+    stack: "stock"
+  },
+  
+  resonance: {
+    name: "共振",
+    group: "buff",
+    stack: "stock"
+  },
+
+  gravity: {
+    name: "重力",
+    group: "debuff",
+    stack: "overwrite"
+  },
+
+  float: {
+    name: "浮力",
+    group: "buff",
+    stack: "overwrite"
+  },
+
+  slow: {
+    name: "減速",
+    group: "debuff",
+    stack: "overwrite"
+  },
+
+  accel: {
+    name: "加速",
+    group: "buff",
+    stack: "overwrite"
+  },
+
+  diffuse: {
+    name: "拡散",
+    group: "buff",
+    stack: "overwrite"
+  },
+
+  converge: {
+    name: "収束",
+    group: "buff",
+    stack: "overwrite"
+  },
+
+  meteor: {
+    name: "流星",
+    group: "buff",
+    stack: "overwrite"
+  },
+
+  satellite: {
+    name: "衛星",
+    group: "buff",
+    stack: "overwrite"
+  }
+
+};
