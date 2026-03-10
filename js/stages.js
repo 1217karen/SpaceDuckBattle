6a7926a43043af13be09d90056280d4a1257bc03
1217karen <k.u.karen.pi@gmail.com>
2026-03-10 15:36:02 +0900

    Update stages.js

--- js/stages.js
+++ js/stages.js
@@ -29,33 +29,11 @@ int:3,
 x:3,
 y:1,
 facing:"W",
-icon:"https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D06.webp",
+icon:"https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D01.webp",
 skills:[
 {type:"attack_nearest"}
 ]
 },
-
-{
-id:"T2",
-name:"チュートリアル敵B",
-team:2,
-role:"defense",
-hp:100,
-mhp:100,
-atk:5,
-df:5,
-heal:0,
-speed:4,
-cri:3,
-int:3,
-x:3,
-y:2,
-facing:"W",
-icon:"https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D08.webp",
-skills:[
-{type:"attack_front1"}
-]
-}
 
 ]
 
