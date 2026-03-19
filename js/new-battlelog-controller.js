c44303fe58d122a702f6a5140ab21c86ff69d8ad
1217karen <k.u.karen.pi@gmail.com>
2026-03-19 11:12:19 +0900

    Update new-battlelog-controller.js

--- js/new-battlelog-controller.js
+++ js/new-battlelog-controller.js
@@ -9,6 +9,7 @@ import {playLogEvent,updateUnitStatUI,updateUnitEffectUI} from "./battlelog-ui.j
 import { skillHandlers } from "./skills.js";
 import { playNextAction } from "./new-battlelog-player.js";
 import { battleState } from "./new-battlelog-state.js";
+import { createLeftSideUI } from "./new-battlelog-ui-init.js";
 
 // =====================
 // DOM取得
@@ -153,21 +154,27 @@ createBoard("board", boardWidth, boardHeight);
 
 if (snapshot) {
   snapshot.units.forEach(u => {
-    battleState.boardState.units[u.id] = {
-      x: u.x,
-      y: u.y,
-      hp: u.hp,
-      mhp: u.mhp ?? u.hp,
-      atk: u.atk ?? 0,
-      def: u.def ?? 0,
-      heal: u.heal ?? 0,
-      speed: u.speed ?? 0,
-      cri: u.cri ?? 0,
-      tec: u.tec ?? 0,
-      effects: [],
-      rateEffects: [],
-      cooldowns: {}   
-    };
+const cooldowns = {};
+
+(u.skills || []).forEach(s => {
+  cooldowns[s.type] = 0;
+});
+
+battleState.boardState.units[u.id] = {
+  x: u.x,
+  y: u.y,
+  hp: u.hp,
+  mhp: u.mhp ?? u.hp,
+  atk: u.atk ?? 0,
+  def: u.def ?? 0,
+  heal: u.heal ?? 0,
+  speed: u.speed ?? 0,
+  cri: u.cri ?? 0,
+  tec: u.tec ?? 0,
+  effects: [],
+  rateEffects: [],
+  cooldowns
+};
 
     placeUnit("board", {
       id: u.id,
@@ -181,156 +188,7 @@ if (snapshot) {
   });
 }
 
-const leftSide = document.getElementById("leftSide");
-
-if (snapshot) {
-  const team1 = snapshot.units.filter(u => u.team === 1);
-
-  team1.forEach(u => {
-    const div = document.createElement("div");
-    div.className = "unitStatus";
-    div.dataset.unit = u.id;
-
-    div.innerHTML = `
-
-<div class="unitHeader">
-  <div class="unitName">${u.name || u.id}</div>
-  <div class="nameDivider"></div>
-</div>
-
-<div class="unitRow">
-
-  <div class="unitMain">
-
-<div class="unitTopRow">
-
-  <img class="statusIcon" src="${u.icon || "https://placehold.co/60x60"}">
-
-  <div class="statusInfoBlock">
-
-    <div class="effectList">
-
-      <div class="effectItem"><span class="effectIcon">浮</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">加</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">共</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">修</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">衛</span><span class="effectCount">00</span></div>
-
-      <div class="effectItem"><span class="effectIcon">重</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">減</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">妨</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">侵</span><span class="effectCount">00</span></div>
-      <div class="effectItem"><span class="effectIcon">流</span><span class="effectCount">00</span></div>
-
-    </div>
-
-    <div class="statDivider"></div>
-
-<div class="statRow">
-
-  <div class="statItem" data-stat="atk">
-    <span class="statLabel">AT</span>
-    <span class="statValue">-</span>
-    <span class="statRate"> </span>
-  </div>
-
-  <div class="statItem" data-stat="def">
-    <span class="statLabel">DF</span>
-    <span class="statValue">-</span>
-    <span class="statRate"> </span>
-  </div>
-
-  <div class="statItem" data-stat="heal">
-    <span class="statLabel">HL</span>
-    <span class="statValue">-</span>
-    <span class="statRate"> </span>
-  </div>
-
-  <div class="statItem" data-stat="speed">
-    <span class="statLabel">SP</span>
-    <span class="statValue">-</span>
-    <span class="statRate"> </span>
-  </div>
-
-  <div class="statItem" data-stat="cri">
-    <span class="statLabel">CR</span>
-    <span class="statValue">-</span>
-    <span class="statRate"> </span>
-  </div>
-
-  <div class="statItem" data-stat="tec">
-    <span class="statLabel">TC</span>
-    <span class="statValue">-</span>
-    <span class="statRate"> </span>
-  </div>
-
-</div>
-
-  </div>
-
-</div>
-
-<div class="hpRow">
-
-  <div class="hpText">
-    HP ${(u.hp ?? u.mhp ?? 0)}/${u.mhp ?? u.hp ?? 0}
-  </div>
-
-  <div class="hpBar">
-    <div class="hpFill" style="width:100%"></div>
-  </div>
-
-</div>
-
-  </div>
-
-  <div class="skillSlots">
-
-<div class="skillSlot"></div>
-<div class="skillSlot"></div>
-
-<div class="skillSlot"></div>
-<div class="skillSlot"></div>
-
-<div class="skillSlot"></div>
-<div class="skillSlot"></div>
-
-<div class="skillSlot"></div>
-<div class="skillSlot"></div>
-  </div>
-
-</div>
-`;
-
-    leftSide.appendChild(div);
-
-    const skillSlots = div.querySelectorAll(".skillSlot");
-
-    (u.skills || []).forEach((s, i) => {
-      const handler = skillHandlers[s.type];
-      if (!handler) return;
-
-      const slot = skillSlots[i];
-      if (!slot) return;
-
-      const img = document.createElement("img");
-      img.src = handler.icon || "";
-      img.style.width = "100%";
-      img.style.height = "100%";
-
-      slot.dataset.skill = s.type;
-      battleState.boardState.units[u.id].cooldowns[s.type] = 0;
-
-      slot.appendChild(img);
-    });
-    updateUnitStatUI(u.id, battleState.boardState);
-
-    const nameEl = div.querySelector(".unitName");
-    if (nameEl) {
-      setTimeout(() => fitUnitName(nameEl), 0);
-    }
-  });
-}
+createLeftSideUI(snapshot, battleState);
 
 // =====================
 // 初期ターンUI
