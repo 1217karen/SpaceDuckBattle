7b184be127e7ed1e87ccb02619da5725a7247ec7
1217karen <k.u.karen.pi@gmail.com>
2026-03-27 12:52:50 +0900

    Update stages.js

--- js/stages.js
+++ js/stages.js
@@ -39,6 +39,43 @@ skills:[
 
 },
 
+normal:{
+
+board:{
+width:7,
+height:5
+},
+
+maxTurns:20,
+
+enemies:[
+
+{
+id:"N1",
+name:"通常敵A",
+team:2,
+role:"attack",
+hp:80,
+mhp:80,
+atk:6,
+def:2,
+heal:0,
+speed:6,
+cri:5,
+tec:3,
+x:6,
+y:2,
+facing:"W",
+icon:"https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D01.webp",
+skills:[
+{type:"attack_front1"}
+]
+},
+
+]
+
+},
+
 test_9x7:{
 
 board:{
