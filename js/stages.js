33c5862974f9f3ebb3571ac361b619b2f527cd7c
1217karen <k.u.karen.pi@gmail.com>
2026-03-27 11:27:23 +0900

    Update stages.js

--- js/stages.js
+++ js/stages.js
@@ -37,6 +37,43 @@ skills:[
 
 ]
 
+},
+
+test_9x7:{
+
+board:{
+width:9,
+height:7
+},
+
+maxTurns:20,
+
+enemies:[
+
+{
+id:"T1",
+name:"テスト敵A",
+team:2,
+role:"attack",
+hp:80,
+mhp:80,
+atk:6,
+def:2,
+heal:0,
+speed:6,
+cri:5,
+tec:3,
+x:8,
+y:3,
+facing:"W",
+icon:"https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D01.webp",
+skills:[
+{type:"attack_front1"}
+]
+},
+
+]
+
 }
 
 };
