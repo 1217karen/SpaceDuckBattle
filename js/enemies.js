99c6a93ac9a9d1fda637a1432f718a7730b0d97e
1217karen <k.u.karen.pi@gmail.com>
2026-03-27 12:57:29 +0900

    Create enemies.js

---
+++ js/enemies.js
@@ -0,0 +1,59 @@
+//enemies.js
+
+export const ENEMIES = {
+
+tutorialEnemyA: {
+team: 2,
+role: "attack",
+hp: 80,
+mhp: 80,
+atk: 10,
+def: 2,
+heal: 0,
+speed: 6,
+cri: 5,
+tec: 0,
+facing: "W",
+icon: "https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D01.webp",
+skills: [
+{ type: "attack_front1" }
+]
+},
+
+normalEnemyA: {
+team: 2,
+role: "attack",
+hp: 80,
+mhp: 80,
+atk: 6,
+def: 2,
+heal: 0,
+speed: 6,
+cri: 5,
+tec: 3,
+facing: "W",
+icon: "https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D01.webp",
+skills: [
+{ type: "attack_front1" }
+]
+},
+
+testEnemyA: {
+team: 2,
+role: "attack",
+hp: 80,
+mhp: 80,
+atk: 0,
+def: 5,
+heal: 5,
+speed: 3,
+cri: 5,
+tec: 3,
+facing: "W",
+icon: "https://www.rabbithutch.site/usagoya/picture.php?user=1217karen&file=D01.webp",
+skills: [
+{ type: "attack_front1" }
+]
+}
+
+};
