2e638e98d7141f4f8b92ccdea6bdd13e32c0f2be
1217karen <k.u.karen.pi@gmail.com>
2026-03-27 13:14:50 +0900

    Update stages.js

--- js/stages.js
+++ js/stages.js
@@ -9,37 +9,39 @@ function makeEnemy(base, extra) {
   };
 }
 
+const BOARDS = {
+small: {
+width: 5,
+height: 4
+},
+medium: {
+width: 7,
+height: 5
+},
+large: {
+width: 8,
+height: 6
+}
+};
+
 export const STAGES = {
 
 tutorial: {
-
-board: {
-width: 4,
-height: 4
-},
-
+board: BOARDS.small,
 maxTurns: 20,
-
 enemies: [
 makeEnemy(ENEMIES.tutorialEnemyA, {
 id: "T1",
 name: "チュートリアル敵A",
-x: 3,
+x: 4,
 y: 1
 })
 ]
-
 },
 
 normal: {
-
-board: {
-width: 7,
-height: 5
-},
-
+board: BOARDS.medium,
 maxTurns: 20,
-
 enemies: [
 makeEnemy(ENEMIES.normalEnemyA, {
 id: "N1",
@@ -48,27 +50,19 @@ x: 6,
 y: 2
 })
 ]
-
 },
 
-test_8x6: {
-
-board: {
-width: 8,
-height: 6
-},
-
+boss: {
+board: BOARDS.large,
 maxTurns: 20,
-
 enemies: [
 makeEnemy(ENEMIES.testEnemyA, {
-id: "T1",
-name: "テスト敵A",
+id: "B1",
+name: "ボス敵A",
 x: 7,
-y: 3
+y: 2
 })
 ]
-
 }
 
 };
