bba5d3fd4280c525fffbb47ed721056d5661ea66
1217karen <k.u.karen.pi@gmail.com>
2026-03-31 14:07:58 +0900

    Update unit-builder.js

--- js/unit-builder.js
+++ js/unit-builder.js
@@ -1,8 +1,8 @@
 //unit-builder.js
 
-export function buildUnitFromDuck(duck, pattern, team, x, y, facing, duckIndex){
+export function buildBattleUnit(unitData, characterData, pattern, team, x, y, facing, unitIndex){
 
-  const stats = duck.stats || {};
+  const stats = unitData.stats || {};
 
   const atk = stats.atk ?? 0;
   const def = stats.def ?? 0;
@@ -18,42 +18,30 @@ export function buildUnitFromDuck(duck, pattern, team, x, y, facing, duckIndex){
     heal * 3 +
     tec * 2 +
     cri * 1 +
-  　speed * 0;
+    speed * 0;
 
-  
-const unit = {
-
-  id: "duck_" + duckIndex,
-
-  name: duck.unitName || "",
-  defaultCharacterName:
-    duck.defaultCharacterName ?? duck.unitName ?? "",
-
-  team: team,
-
-  role: duck.type,
-
-  hp: mhp,
-  mhp: mhp,
-
-  atk: atk,
-  def: def,
-  heal: heal,
-  tec: tec,
-  speed: speed,
-  cri: cri,
-
-  x: x,
-  y: y,
-
-  facing: facing,
-
-  icon: duck.icon?.default ?? "",
-
-  patterns: duck.patterns || [],
-  commIcons: duck.commIcons || [],
-  commDialogues: duck.commDialogues || {},
-
+  const unit = {
+    id: "unit_" + unitIndex,
+    name: unitData.name || "",
+    defaultCharacterName:
+      characterData?.defaultName ?? "",
+    team: team,
+    role: unitData.type,
+    hp: mhp,
+    mhp: mhp,
+    atk: atk,
+    def: def,
+    heal: heal,
+    tec: tec,
+    speed: speed,
+    cri: cri,
+    x: x,
+    y: y,
+    facing: facing,
+    icon: unitData.icon?.default ?? "",
+    patterns: unitData.patterns || [],
+    commIcons: characterData?.commIcons || [],
+    commDialogues: characterData?.commDialogues || {},
   skills: (pattern.skills || [])
     .filter(s => {
       if (typeof s === "string") {
