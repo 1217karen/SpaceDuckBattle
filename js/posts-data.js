c63a9c0b1604af38872554c71a8be2f32c7878ed
1217karen <k.u.karen.pi@gmail.com>
2026-04-07 13:25:48 +0900

    Create posts-data.js

---
+++ js/posts-data.js
@@ -0,0 +1,115 @@
+//posts-data.js
+
+export const posts = [
+  {
+    postId: 1,
+    placeId: "F1",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはF1です",
+    createdAt: "2026-04-07 10:00",
+    authorEno: 0
+  },
+  {
+    postId: 2,
+    placeId: "F1-2",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはF1-2です",
+    createdAt: "2026-04-07 10:01",
+    authorEno: 0
+  },
+  {
+    postId: 3,
+    placeId: "F1-3",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはF1-3です",
+    createdAt: "2026-04-07 10:02",
+    authorEno: 0
+  },
+
+  {
+    postId: 4,
+    placeId: "E1",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはE1です",
+    createdAt: "2026-04-07 10:03",
+    authorEno: 0
+  },
+  {
+    postId: 5,
+    placeId: "E1-2",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはE1-2です",
+    createdAt: "2026-04-07 10:04",
+    authorEno: 0
+  },
+  {
+    postId: 6,
+    placeId: "E1-3",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはE1-3です",
+    createdAt: "2026-04-07 10:05",
+    authorEno: 0
+  },
+
+  {
+    postId: 7,
+    placeId: "E2",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはE2です",
+    createdAt: "2026-04-07 10:06",
+    authorEno: 0
+  },
+  {
+    postId: 8,
+    placeId: "E2-2",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはE2-2です",
+    createdAt: "2026-04-07 10:07",
+    authorEno: 0
+  },
+  {
+    postId: 9,
+    placeId: "E2-3",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはE2-3です",
+    createdAt: "2026-04-07 10:08",
+    authorEno: 0
+  },
+
+  {
+    postId: 10,
+    placeId: "R1",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはR1です",
+    createdAt: "2026-04-07 10:09",
+    authorEno: 0
+  },
+  {
+    postId: 11,
+    placeId: "R2",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはR2です",
+    createdAt: "2026-04-07 10:10",
+    authorEno: 0
+  },
+  {
+    postId: 12,
+    placeId: "R3",
+    speakerName: "テスト",
+    iconId: null,
+    body: "ここはR3です",
+    createdAt: "2026-04-07 10:11",
+    authorEno: 0
+  }
+];
