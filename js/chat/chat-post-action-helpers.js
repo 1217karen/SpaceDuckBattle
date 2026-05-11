9be8a7873cbc8656fd3bc7498411b5405cc688f1
1217karen <k.u.karen.pi@gmail.com>
2026-05-11 15:46:18 +0900

    Update chat-post-action-helpers.js

--- js/chat/chat-post-action-helpers.js
+++ js/chat/chat-post-action-helpers.js
@@ -82,3 +82,28 @@ export function createQuoteHandler({ composerRefs }) {
     textarea.dispatchEvent(new Event("input", { bubbles: true }));
   };
 }
+
+export function createPostActions(actions = {}) {
+  return {
+    onReply:
+      typeof actions.onReply === "function"
+        ? actions.onReply
+        : null,
+    onDelete:
+      typeof actions.onDelete === "function"
+        ? actions.onDelete
+        : null,
+    onOpenThread:
+      typeof actions.onOpenThread === "function"
+        ? actions.onOpenThread
+        : null,
+    onQuote:
+      typeof actions.onQuote === "function"
+        ? actions.onQuote
+        : null,
+    onHide:
+      typeof actions.onHide === "function"
+        ? actions.onHide
+        : null
+  };
+}
