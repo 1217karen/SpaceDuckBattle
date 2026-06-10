c77bd34885c6c345621da71019ce690febf9ec43
1217karen <k.u.karen.pi@gmail.com>
2026-06-10 17:53:48 +0900

    Update chat-thread-controller.js

--- js/chat/chat-thread-controller.js
+++ js/chat/chat-thread-controller.js
@@ -6,7 +6,8 @@ import { getPlaceById, getPlaceLabel, getFavoritePlaces } from "./chat-place-uti
 import { getCurrentAccount, loadCharacter } from "../services/storage-service.js";
 import { createIconPicker } from "../common/icon-picker.js";
 import { createPost,getAllPosts} from "../services/post-service.js";
-import { renderThreadHeaderSection,renderChatComposerSection } from "./chat-view.js";
+import { renderThreadHeaderSection } from "./chat-view.js";
+import { renderChatComposerSection } from "./chat-composer-view.js";
 import { createPostCard,renderPostListSection,renderPostListContent } from "./chat-post-view.js";
 import { loadComposerDraft,saveComposerDraft,readComposerDraftFromRefs} from "./chat-composer-state.js";
 import { createReplyStateFromPost,clearReplyState,applyReplyStateToDraft,findReplySourcePost} from "./chat-reply-state.js";
