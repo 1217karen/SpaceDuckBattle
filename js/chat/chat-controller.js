b50e13d403d7ba3d25c840b2c4fd3097efd01b2a
1217karen <k.u.karen.pi@gmail.com>
2026-06-10 17:15:32 +0900

    Update chat-controller.js

--- js/chat/chat-controller.js
+++ js/chat/chat-controller.js
@@ -16,7 +16,7 @@ import { getThreadRootPostIdFromQuery,getThreadPosts } from "./chat-thread-view.
 import { showToast } from "../common/toast.js";
 import { isFavoritePlace,toggleFavoritePlace } from "./chat-place-favorites.js";
 import { setupRenderedComposer, getFixedReplyTargetName } from "./chat-composer-ui.js";
-import { renderFavoritePlacesSidePanel } from "./chat-side-panel.js";
+import { renderFavoritePlacesSidePanel } from "./chat-favorites-panel.js";
 import { createPostActions,openThreadFromPost,getReplyTargetLabels,createDeleteHandler,createHideHandler,createQuoteHandler,getQuotePreviewPostById } from "./chat-post-action-helpers.js";
 
 
