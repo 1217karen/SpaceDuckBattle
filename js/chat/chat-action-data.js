a64fdab772d5c964e5186b4865d6b8d3c87b16b9
1217karen <k.u.karen.pi@gmail.com>
2026-06-15 09:31:13 +0900

    Create chat-action-data.js

---
+++ js/chat/chat-action-data.js
@@ -0,0 +1,10 @@
+// chat-action-data.js
+
+export const chatActionDefinitions = [
+  {
+    actionId: "look-around",
+    label: "周辺を見る",
+    type: "common",
+    resultText: "{name}は周囲を見回した。ここは{placeName}だ。"
+  }
+];
