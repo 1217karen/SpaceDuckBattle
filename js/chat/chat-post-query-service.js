b7b7e7c65d4223fbb77971f3a16bc227b9215a56
1217karen <k.u.karen.pi@gmail.com>
2026-06-17 09:53:56 +0900

    Update chat-post-query-service.js

--- js/chat/chat-post-query-service.js
+++ js/chat/chat-post-query-service.js
@@ -4,29 +4,65 @@ import { getAllPosts } from "./post-service.js";
 import { getDisplayPosts } from "../chat/chat-display-rules.js";
 import { getHerePosts, getReplyPostsForEno, getSelfPostsForEno } from "../chat/chat-post-filter.js";
 
+export function getChatTimelinePosts({
+  currentPlace = null,
+  places = [],
+  viewerEno = null
+} = {}) {
+  return getDisplayPosts({
+    currentPlace,
+    allPosts: getAllPosts(),
+    places,
+    viewerEno
+  });
+}
+
+export function getReplyPostsForViewer({
+  viewerEno = null
+} = {}) {
+  return getReplyPostsForEno(getAllPosts(), viewerEno);
+}
+
+export function getSelfPostsForViewer({
+  viewerEno = null
+} = {}) {
+  return getSelfPostsForEno(getAllPosts(), viewerEno);
+}
+
+export function getHerePostsForPlace({
+  currentPlace = null,
+  viewerEno = null
+} = {}) {
+  return getHerePosts(currentPlace, getAllPosts(), viewerEno);
+}
+
 export function getChatPostsForViewMode({
   viewMode = "chat",
   currentPlace = null,
   places = [],
   viewerEno = null
 } = {}) {
-  const allPosts = getAllPosts();
-
   if (viewMode === "reply") {
-    return getReplyPostsForEno(allPosts, viewerEno);
+    return getReplyPostsForViewer({
+      viewerEno
+    });
   }
 
   if (viewMode === "self") {
-    return getSelfPostsForEno(allPosts, viewerEno);
+    return getSelfPostsForViewer({
+      viewerEno
+    });
   }
 
   if (viewMode === "here") {
-    return getHerePosts(currentPlace, allPosts, viewerEno);
+    return getHerePostsForPlace({
+      currentPlace,
+      viewerEno
+    });
   }
 
-  return getDisplayPosts({
+  return getChatTimelinePosts({
     currentPlace,
-    allPosts,
     places,
     viewerEno
   });
